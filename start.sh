a4fd6f85dc5588d786e321184c591537500fffda
lincolnthedev <github@lincolnwilliams.com>
2021-06-25 14:08:41 -0400

    Added Start Script

---
+++ start.sh
@@ -0,0 +1,6 @@
+clear
+figlet -f small kaori
+printf "\n\nOpen Source Universal Remote with Raspberry Pi\nBy lincolnthedev\n\n"
+cd ./public
+printf "Starting HTTP server now...\n\n"
+sudo php -S 0.0.0.0:80
